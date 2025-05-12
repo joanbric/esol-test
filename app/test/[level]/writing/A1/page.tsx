a1821449a25c6c1f1f808af64f6969be06d8326f
Jhon Briones <joanbric@gmail.com>
2025-05-12 18:40:41 +0100

    Clerk installed and set up

---
+++ app/test/[level]/writing/A1/page.tsx
@@ -0,0 +1,21 @@
+export default function WritingA1Page({
+  params
+}: {
+  params: { level: string }
+}) {
+  return (
+    <>
+      <h1>{params.level} Writing Activity 1</h1>
+      <h2>Hobbies and Interests</h2>
+
+      <ol>
+        <li>
+         
+        </li>
+        <li>
+        
+        </li>
+      </ol>
+    </>
+  )
+}
